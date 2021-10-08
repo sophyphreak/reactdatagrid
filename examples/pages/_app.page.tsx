49868a94142fde7634f754b88d96a3f2024d0f19
inovua-admin <dev@reactdatagrid.io>
2021-10-08 10:28:55 +0300

    Add example with performance issues

--- examples/pages/_app.page.tsx
+++ examples/pages/_app.page.tsx
@@ -10,6 +10,8 @@ import '../../community-edition/style/base.scss';
 import '../../community-edition/style/theme/default-light/index.scss';
 import '../../community-edition/style/theme/default-dark/index.scss';
 
+import '../pages/prop-checkboxColumn/prop-checkboxColumns.scss';
+
 export default function MyApp({ Component, pageProps }) {
   if (!(process as any).browser) {
     return null;
