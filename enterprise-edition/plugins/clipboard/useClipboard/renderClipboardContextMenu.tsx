93481aaf54eec85112d1fb193474846c4b7ccb66
inovua-admin <dev@reactdatagrid.io>
2021-11-18 00:51:23 +0200

    update

--- enterprise-edition/plugins/clipboard/useClipboard/renderClipboardContextMenu.tsx
+++ enterprise-edition/plugins/clipboard/useClipboard/renderClipboardContextMenu.tsx
@@ -54,12 +54,12 @@ const renderClipboardContextMenu = (
   menuProps.items = [
     {
       label: 'Copy',
-      secondaryLabel: 'Ctrl/Cmd + c',
+      secondaryLabel: 'Ctrl/Cmd + C',
       onClick: () => onCopyClickHandle(computedProps),
     },
     {
       label: 'Paste',
-      secondaryLabel: 'Ctrl/Cmd + v',
+      secondaryLabel: 'Ctrl/Cmd + V',
       onClick: () => onPasteClickHandle(computedProps),
       disabled: pasteDisableHandle(computedProps),
     },
