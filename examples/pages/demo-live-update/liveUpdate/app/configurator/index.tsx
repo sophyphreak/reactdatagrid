912ff9ea5f058c27c2ed3bccf0aa8031a312116e
inovua-admin <dev@reactdatagrid.io>
2022-03-16 10:03:50 +0200

    update live demo

--- examples/pages/demo-live-update/liveUpdate/app/configurator/index.tsx
+++ examples/pages/demo-live-update/liveUpdate/app/configurator/index.tsx
@@ -5,6 +5,13 @@ import buildDataSource from '../grid/dataSource';
 
 import FieldWithLabel from '../../components/FieldWithLabel';
 
+const closeIcon = (
+  <svg height="24px" viewBox="0 0 24 24" width="24px">
+    <path d="M0 0h24v24H0V0z" fill="none" />
+    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
+  </svg>
+);
+
 const recordsDataSource = [
   { id: 1000, label: '1000' },
   { id: 5000, label: '5000' },
@@ -40,8 +47,13 @@ const Configurator = (props: any) => {
   const [reload, setReload] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const { loadDataSource, setRecords, setTimer, setUpdateRecords, setLoad } =
-    useAppActions();
+  const {
+    loadDataSource,
+    setRecords,
+    setTimer,
+    setUpdateRecords,
+    setLoad,
+  } = useAppActions();
   const { cols, times, records, interval, data, updateRecords } = useAppState();
 
   useEffect(() => {
@@ -193,6 +205,13 @@ const Configurator = (props: any) => {
   return (
     <div className="configurator">
       <div className="configurator-title">Configurator</div>
+
+      <button
+        className="configurator-close-button"
+        onClick={() => props.setVisible(false)}
+      >
+        {closeIcon}
+      </button>
 
       <FieldWithLabel
         type="select"
--- examples/pages/demo-live-update/liveUpdate/app/index.tsx
+++ examples/pages/demo-live-update/liveUpdate/app/index.tsx
@@ -3,14 +3,31 @@ import React, { useState } from 'react';
 import Grid from './grid';
 import Configurator from './configurator';
 
+const menuIcon = (
+  <svg height="24px" viewBox="0 0 24 24" width="24px">
+    <path d="M0 0h24v24H0V0z" fill="none" />
+    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+  </svg>
+);
+
 const App = () => {
   const [gridRef, setGridRef] = useState(null);
+  const [visible, setVisible] = useState(false);
 
   return (
     <div className="app">
       <div className="app-content">
         <Grid setGridRef={setGridRef} />
-        <Configurator gridRef={gridRef} />
+        {visible ? (
+          <Configurator gridRef={gridRef} setVisible={setVisible} />
+        ) : (
+          <button
+            className="app-content-menu-button"
+            onClick={() => setVisible(true)}
+          >
+            {menuIcon}
+          </button>
+        )}
       </div>
     </div>
   );
